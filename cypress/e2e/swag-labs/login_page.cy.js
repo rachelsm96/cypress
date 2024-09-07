885d4da38d64b2ecd3c03310f522b0ddf04664df
Rachel <martinez19x96@gmail.com>
2024-09-07 16:16:52 -0500

    add more login tests

--- cypress/e2e/swag-labs/login_page.cy.js
+++ cypress/e2e/swag-labs/login_page.cy.js
@@ -1,24 +1,36 @@
 /// <reference types="cypress" />
 
-context('go to login', () => {
+context('login page', () => {
     beforeEach(() => {
       cy.visit('https://www.saucedemo.com', {failOnStatusCode: false} )
     })
 
     it('login with standard user', () => {
-      // https://on.cypress.io/type
-      cy.get('[id="user-name"]').type('locked_out_user')
+      cy.get('[id="user-name"]').type('standard_user')
       cy.get('[id="password"]').type('secret_sauce')
       cy.get('[id="login-button"]').click()
     })
 
     it('login with locked out user', () => {
-        // https://on.cypress.io/type
         cy.get('[id="user-name"]').type('locked_out_user')
         cy.get('[id="password"]').type('secret_sauce')
         cy.get('[id="login-button"]').click()
         cy.get('.error-message-container').should('have.text', 'Epic sadface: Sorry, this user has been locked out.')
-})
+    })
+
+    it('invalid password', () => {
+      cy.get('[id="user-name"]').type('standard_user')
+      cy.get('[id="password"]').type('password')
+      cy.get('[id="login-button"]').click()
+      cy.get('.error-message-container').should('have.text', 'Epic sadface: Username and password do not match any user in this service')
+    })
+
+    it('invalid user', () => {
+      cy.get('[id="user-name"]').type('not_a_user')
+      cy.get('[id="password"]').type('secret_sauce')
+      cy.get('[id="login-button"]').click()
+      cy.get('.error-message-container').should('have.text', 'Epic sadface: Username and password do not match any user in this service')
+    })
     
 
 })
